b5f3d587cb231ca754b211623d520e76d3ea6d1f
Tim Jarratt <tim.jarratt@maersk.com>
2023-11-21 09:16:57 +0100

    Add cloc package

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -22,6 +22,8 @@
     # # "Hello, world!" when run.
     # pkgs.hello
 
+    pkgs.cloc
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
