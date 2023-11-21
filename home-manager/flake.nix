e53665e967d84812da708661ea7127e807bf3be0
Tim Jarratt <tim.jarratt@maersk.com>
2023-11-21 09:03:53 +0100

    Home manager changes as of 21-11-2023T09:03:00

---
+++ home-manager/flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Home Manager configuration of tim.jarratt";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/master";
+      follows = "nrg/nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nrg = {
+      url = "/Users/tim.jarratt/workspace/nrg";
+    };
+  };
+
+  outputs =
+    { nixpkgs
+    , home-manager
+    , nrg
+    , ...
+    }:
+    let
+      system = "aarch64-darwin";
+      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+    in
+    {
+      homeConfigurations."tim.jarratt" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix nrg.homeManager ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+        extraSpecialArgs = {
+          fullName = "Tim Jarratt";
+          emailAddress = "tim.jarratt@maersk.com";
+        };
+      };
+    };
+}
