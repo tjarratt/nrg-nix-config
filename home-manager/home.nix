8108b6a2f9c66597763de6bb04ff32890fc3fd0e
Tim Jarratt <tim.jarratt@maersk.com>
2023-12-07 16:11:02 +0100

    Install flyctl

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -23,6 +23,7 @@
     # pkgs.hello
 
     pkgs.cloc
+    pkgs.flyctl
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
