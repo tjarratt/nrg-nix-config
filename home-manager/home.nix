6ade97289a5a916f990929656ffda64746367dfd
Tim Jarratt <tim.jarratt@maersk.com>
2023-12-13 10:15:36 +0100

    Add azure cli
    Co-authored-by: Andrew Bruce <andrew.bruce@maersk.com>

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -24,6 +24,7 @@
 
     pkgs.cloc
     pkgs.flyctl
+    pkgs.azure-cli
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
